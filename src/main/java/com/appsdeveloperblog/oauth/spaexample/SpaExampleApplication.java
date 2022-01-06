86ebe302b5146455551bd281ba245a79b77852c6
shabloel <shabloel@outlook.com>
2022-01-06 23:21:44 +0100

    first commit

---
+++ src/main/java/com/appsdeveloperblog/oauth/spaexample/SpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.oauth.spaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaExampleApplication.class, args);
+    }
+
+}
